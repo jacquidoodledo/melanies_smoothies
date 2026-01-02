9ab186f8edd2cec085cf1a7bf9aee85ca492bf2e
Jacqui Smith <66119456+jacquidoodledo@users.noreply.github.com>
2026-01-02 17:59:24 +0000

    Added page titles to sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,6 @@ st.set_page_config(
     page_icon="👋",
 )
 
-pages = [
-    Page("pages/order_request_form.py", "Home", "🏠"),
-    Page("pages/order_fulfillment.py", "Fulfillment", "📊"),
-]
-
 st.write("# Welcome to Streamlit! 👋")
 
 st.sidebar.success("Select a page above.")
